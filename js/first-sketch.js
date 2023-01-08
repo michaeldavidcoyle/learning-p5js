df997499d820530f7746113a8a14d3b79bb2bc35
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-07 21:06:42 -0600

    add each of the 2D primitive shapes

--- js/first-sketch.js
+++ js/first-sketch.js
@@ -6,5 +6,18 @@ function setup() {
 
 function draw() {
     background(220);
-    ellipse(50, 50, 80, 80);
+    strokeWeight(1);
+    arc(200, 50, 80, 80, 0, Math.PI);
+    ellipse(60, 50, 100, 80);
+    circle(300, 50, 40);
+    line(50, 120, 300, 150);
+    strokeWeight(3);
+    for (let x = 1; x < 5; x++) {
+        point(x * 80, 200);
+    }
+    strokeWeight(1);
+    quad(30, 232, 111, 243, 107, 295, 40, 270);
+    rect(180, 250, 80, 55);
+    square(300, 275, 70);
+    triangle(35, 310, 80, 370, 180, 388);
 }
