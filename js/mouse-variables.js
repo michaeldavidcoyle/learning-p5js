c6b831fde03f8c8ed17d5a96316bf16d80e1630c
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-09 22:44:23 -0600

    add ability to erase

--- js/mouse-variables.js
+++ js/mouse-variables.js
@@ -3,12 +3,18 @@
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(255);
-    stroke(0);
-    strokeWeight(2);
 }
 
 function draw() {
     if (mouseIsPressed) {
+        stroke(0);
+        strokeWeight(2);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+
+    if (keyIsPressed) {
+        stroke(255);
+        strokeWeight(8);
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
 }
