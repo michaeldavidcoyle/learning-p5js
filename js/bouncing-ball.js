e7e61acfd6e952f6f33ab3e18d4522d23c816dcf
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-10 20:20:05 -0600

    add condition to make ball bounce back off side walls

--- js/bouncing-ball.js
+++ js/bouncing-ball.js
@@ -1,8 +1,8 @@
 let x = 100;
 let y = Math.round(window.innerHeight / 2);
 let d = window.innerWidth / 16;
+let r = d / 2;
 let xSpeed = Math.round(window.innerWidth / 100);
-console.log(xSpeed);
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
@@ -16,4 +16,8 @@ function draw() {
     circle(x, y, d);
 
     x += xSpeed;
+
+    if (x - r < 0 || x + r > width) {
+        xSpeed *= -1;
+    }
 }
