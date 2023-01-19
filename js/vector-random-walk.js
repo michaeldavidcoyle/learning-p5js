4236a9bff29488235291802b1f5313dafb357a34
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-19 00:22:35 -0600

    add multiple walkers with variation in stroke value and weight

--- js/vector-random-walk.js
+++ js/vector-random-walk.js
@@ -1,18 +1,22 @@
-let walker;
+let walkers = [];
 let previous;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    let p = createVector(width / 2, height / 2);
-    let v = p5.Vector.random2D();
-    v.setMag(random(1, 16));
-    walker = new Walker(p, v);
-    background(247);
+    for (let i = 0; i < 16; i++) {
+        let p = createVector(width / 2, height / 2);
+        let v = p5.Vector.random2D();
+        v.setMag(random(1, 16));
+        walkers.push(new Walker(p, v));
+    }
+    background(128);
 }
 
 function draw() {
-    walker.render();
-    walker.update();
+    walkers.forEach(walker => {
+        walker.render();
+        walker.update();
+    });
 }
 
 function Walker(pos, vel) {
@@ -20,9 +24,13 @@ function Walker(pos, vel) {
     this.vel = vel;
     this.prev = pos.copy();
     this.step = min(width, height) / 32;
+    this.gray = floor(random(0, 255));
+    this.weight = 1;
+    this.weightStep = random(-1, 1);
 
     this.render = () => {
-        stroke(64);
+        stroke(this.gray);
+        strokeWeight(this.weight);
         line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
     }
 
@@ -31,5 +39,11 @@ function Walker(pos, vel) {
         this.pos.add(this.vel);
         this.vel = p5.Vector.random2D();
         this.vel.setMag(random(1, this.step));
+
+        this.weight += this.weightStep;
+
+        if (this.weight < 1 || this.weight > 5) {
+            this.weightStep *= -1;
+        }
     }
 }
