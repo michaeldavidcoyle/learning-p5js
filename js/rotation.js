ef2fda3c8163070302515c3635f0f0cebae2dbd0
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-26 00:22:39 -0600

    add colored pie wedges to background

--- js/rotation.js
+++ js/rotation.js
@@ -2,16 +2,29 @@ let angle = 0;
 let angleVelocity = 0;
 let angleAcceleration = 0.0002;
 let friction = 0.99;
+let arcW;
+let turn = Math.PI / 3;
 
 function setup() {
     createCanvas(400, 400);
     rectMode(CENTER);
+    arcW = width * 0.9;
+    colorMode(HSL, 360, 100, 100);
 }
 
 function draw() {
     background(223);
 
     translate(width / 2, height / 2);
+    // draw colored pie wedges
+    for (
+        let start = 0, stop = turn;
+        start < TAU, stop <= TAU;
+        start += turn, stop += turn
+    ) {
+        fill(degrees(start), 100, 48);
+        arc(0, 0, arcW, arcW, start, stop);
+    }
     rotate(angle);
 
     arrow();
@@ -33,7 +46,7 @@ function arrow() {
     let h = height / 32;
     let end = w / 2;
     noStroke();
-    fill(223, 64, 32);
+    fill(0, 0, 20);
     rect(0, 0, w, h);
     triangle(end + h, 0, end - h, h, end - h, -h);
 }
