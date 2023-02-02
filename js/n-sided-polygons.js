21f31dfccd9d668de5e4af655fc504a7b4188db7
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-01 19:49:18 -0600

    create coordinates function and use for loop to create n-sided polygon

--- js/n-sided-polygons.js
+++ js/n-sided-polygons.js
@@ -1,7 +1,31 @@
+let sideCount = 8;
+let step;
+let radius;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    radius = min(width, height) / 3;
+    step = TAU / sideCount;
 }
 
 function draw() {
-    background(128);
+    background(223);
+
+    beginShape();
+    for (let angle = 0; angle < TAU; angle += step) {
+        let p = coordinates(
+            {x: width / 2, y: height / 2},
+            radius,
+            angle
+        );
+        vertex(p.x, p.y);
+    }
+    endShape(CLOSE);
+}
+
+function coordinates(origin, radius, angle) {
+    return {
+        x: origin.x + cos(angle) * radius,
+        y: origin.y + sin(angle) * radius
+    }
 }
