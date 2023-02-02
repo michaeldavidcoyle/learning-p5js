4731e01f923d9f6ade1ab72bfa4a6f8ca5808198
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-01 20:42:52 -0600

    set number of sides, radius and start angle according to user input and move shape code to polygon function

--- js/n-sided-polygons.js
+++ js/n-sided-polygons.js
@@ -2,22 +2,32 @@ const sidesInput = document.querySelector('#side-count');
 const radiusInput = document.querySelector('#radius');
 const angleInput = document.querySelector('#start-angle');
 
-let start = -Math.PI / 2;
+let start = 0;
 let stop;
-let sideCount = 5;
+let sideCount = 3;
 let step;
 let radius;
 
 function setup() {
     createCanvas(innerWidth, innerHeight * 0.75);
-    stop = start + TAU;
-    radius = min(width, height) / 3;
-    step = TAU / sideCount;
+    radius = 100;
 }
 
 function draw() {
     background(223);
+    polygon(sideCount, radians(start), radius);
+}
 
+function coordinates(origin, radius, angle) {
+    return {
+        x: origin.x + cos(angle) * radius,
+        y: origin.y + sin(angle) * radius
+    }
+}
+
+function polygon(sides, start, radius) {
+    stop = start + TAU;
+    step = TAU / sides;
     beginShape();
     for (let angle = start; angle < stop; angle += step) {
         let p = coordinates(
@@ -30,21 +40,14 @@ function draw() {
     endShape(CLOSE);
 }
 
-function coordinates(origin, radius, angle) {
-    return {
-        x: origin.x + cos(angle) * radius,
-        y: origin.y + sin(angle) * radius
-    }
-}
-
 sidesInput.addEventListener('change', event => {
-    console.log(`sides: ${event.target.value}`);
+    sideCount = event.target.value;
 });
 
 radiusInput.addEventListener('change', event => {
-    console.log(`radius: ${event.target.value}`);
+    radius = event.target.value;
 });
 
 angleInput.addEventListener('change', event => {
-    console.log(`angle: ${event.target.value}`);
+    start = event.target.value;
 });
