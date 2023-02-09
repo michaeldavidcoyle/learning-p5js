050db698abf47cba0df53ad1af5ff262f60ff0a9
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-08 22:16:36 -0600

    grab form and inputs from DOM, add event listener on form, set input variables and redraw pattern

--- js/fraction-turn.js
+++ js/fraction-turn.js
@@ -1,13 +1,17 @@
 // Inspired by Numberphile's The Golden Ratio (why it is so irrational)
 // https://www.youtube.com/watch?v=sj8Sg8qnjOg
 
+const form = document.querySelector('form');
+const fractionInput = document.querySelector('#fraction');
+const seedSizeInput = document.querySelector('#seed-size');
+const finalRadiusInput = document.querySelector('#final-radius');
+
 const midpoint = {
     x: null,
     y: null
 }
 
-let x,
-    y,
+let seedSize,
     angle,
     radius,
     factor,
@@ -20,23 +24,33 @@ function setup() {
     midpoint.y = height / 2;
     angle = 0;
     radius = 0;
-    factor = (1 + sqrt(5)) / 2;
-    turn = TAU * factor;
-    end = dist(0, 0, midpoint.x, midpoint.y);
 
     stroke(80, 80, 64)
-    fill(223, 168, 32);
+    fill(128, 128, 98);
 }
 
 function draw() {
-    noLoop();
     background(198, 165, 133);
-    for (let radius = 0; radius < end; radius += 0.5) {
+    for (let radius = 0; radius < end; radius += 0.1) {
         x = midpoint.x + sin(angle) * radius;
         y = midpoint.y + cos(angle) * radius;
 
-        circle(x, y, 16);
+        circle(x, y, seedSize);
 
         angle += turn;
     }
+    noLoop();
 }
+
+function setFactors(fraction, size, finalRadius) {
+    factor = fraction;
+    turn = TAU * factor;
+    seedSize = size;
+    end = finalRadius;
+}
+
+form.addEventListener('submit', event => {
+   event.preventDefault();
+   setFactors(fractionInput.value, seedSizeInput.value, finalRadiusInput.value);
+   redraw();
+});
