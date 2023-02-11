e225e1059e22af8e9781c5cb31bd891744c6528e
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-11 16:01:27 -0600

    add condition to stop draw loop and set canvas height/width based on orientation

--- js/fraction-turn-animation.js
+++ js/fraction-turn-animation.js
@@ -19,13 +19,24 @@ function setup() {
     midpoint.y = height / 2;
     angle = 0;
     factor = 0;
-    finalRadius = dist(0, 0, midpoint.x, midpoint.y);
+
+    if (width > height) {
+        // set final radius to half the width
+        finalRadius = dist(midpoint.x, midpoint.y, midpoint.x, height);
+    } else {
+        // set final radius to half the height
+        finalRadius = dist(midpoint.x, midpoint.y, width, midpoint.y);
+    }
 
     stroke(80, 80, 64);
     textSize(24);
 }
 
 function draw() {
+    if (factor > 999999) {
+        noLoop();
+    }
+
     background(198, 165, 133);
 
     let fraction = factor / 1e6;
