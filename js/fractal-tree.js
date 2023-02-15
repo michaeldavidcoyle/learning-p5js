16208b8a405731b303bc99dc0d8b958a794b1844
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-14 23:47:31 -0600

    introduce some randomness to the tree growth

--- js/fractal-tree.js
+++ js/fractal-tree.js
@@ -1,11 +1,12 @@
 let turn = Math.PI / 6;
 let x, y;
 let count = 0;
+let angleOffset = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     x = width / 2;
-    y = height - height / 5;
+    y = height - height / 10;
 }
 
 function draw() {
@@ -21,10 +22,13 @@ function branch(x, y, length, angle, weight) {
     line(x, y, endX, endY);
 
     if (length > 3) {
-        let nextLength = length * 0.75;
-        let angleA = angle + turn;
-        let angleB = angle - turn;
+        let nextLength = length * random(0.67, 0.875);
+        let angleA = angle + turn * noise(angleOffset);
+        let angleB = angle - turn * noise(angleOffset);
         let nextWeight = weight * 0.75;
+
+        angleOffset += 0.001;
+
         branch(endX, endY, nextLength, angleA, nextWeight);
         branch(endX, endY, nextLength, angleB, nextWeight);
     }
