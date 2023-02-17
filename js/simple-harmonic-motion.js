402d699a0fd250273346b680a83ef2d43949edbc
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-16 18:26:12 -0600

    create array of starting angles so as to render oscillating wave

--- js/simple-harmonic-motion.js
+++ js/simple-harmonic-motion.js
@@ -1,23 +1,29 @@
-let angle = 0;
-let angleV;
+let angles = [];
+let angleVs = [];
 let diameter;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleV = TAU / 128;
-    diameter = 64;
+    diameter = 16;
+    let total = width / diameter;
+    for (let i = 0; i <= total; i++) {
+        angles.push(map(i, 0, total, 0, TAU * 2));
+        angleVs.push(0.1 + i / 100);
+    }
+    stroke(160, 160, 144);
+    strokeWeight(4);
+    fill(160, 160, 144);
+    // noFill();
 }
 
 function draw() {
     background(32);
 
-    let y = map(sin(angle), -1, 1, height - 50, 50);
-
-    stroke(160, 160, 128);
-    strokeWeight(4);
-    fill(160, 160, 128);
-    line(width / 2, height / 2, width / 2, y);
-    circle(width / 2, y, diameter);
-
-    angle += angleV;
+    for (let i = 0; i < angles.length; i++) {
+        let y = map(sin(angles[i]), -1, 1, height - 100, 100);
+        let x = map(i, 0, angles.length, 0, width);
+        circle(x, y, diameter);
+        angles[i] += 0.01;
+    }
 }
