b36b844c18ee431b8891970e1ca64e2d0af77006
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-17 21:06:27 -0600

    make array of waves to make one additive wave

--- js/simple-harmonic-motion.js
+++ js/simple-harmonic-motion.js
@@ -1,10 +1,14 @@
 let diameter;
-let wave;
+let waves = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    diameter = 16;
-    wave = new Wave(height / 4, width / 2, 0);
+    diameter = 10;
+    for (let i = 0; i < 5; i++) {
+        waves.push(
+            new Wave(random(height/4), random(width/10, width/2), random(width/3))
+        );
+    }
     stroke(160, 160, 144);
     strokeWeight(4);
     fill(160, 160, 144);
@@ -14,7 +18,10 @@ function draw() {
     background(32);
 
     for (let x = 0; x < width; x += 10) {
-        let y = wave.evaluate(x);
+        let y = 0;
+        waves.forEach(wave => {
+            y += wave.evaluate(x);
+        });
         circle(x, y + height / 2, diameter);
     }
 }
