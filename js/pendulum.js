5bf7d60454383764e161a441108801c40671b526
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-20 21:00:54 -0600

    add variables for angle, velocity and position of bob

--- js/pendulum.js
+++ js/pendulum.js
@@ -1,12 +1,31 @@
+let angle;
+let bob;
+let length;
+let angleV;
+let angleA;
+let origin;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+
     stroke(64);
-    strokeWeight(4);
+    strokeWeight(8);
+    fill(64);
+
+    angle = PI / 4;
+    bob = createVector();
+    length = height * 0.67;
+    angleV = 0;
+    angleA = 0;
+    origin = createVector(width / 2, 0);
 }
 
 function draw() {
     background(223);
 
-    line(width / 2, 0, width / 2, height - height / 4);
-    circle(width / 2, height - height / 4, 32);
+    bob.x = length * sin(angle) + origin.x;
+    bob.y = length * cos(angle) + origin.y;
+
+    line(origin.x, origin.y, bob.x, bob.y);
+    circle(bob.x, bob.y, 64);
 }
