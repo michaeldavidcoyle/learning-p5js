b37b46c8c1f2f7236f339b47e32d252b102104cf
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-21 18:24:10 -0600

    convert to object-oriented

--- js/pendulum.js
+++ js/pendulum.js
@@ -1,37 +1,43 @@
-let angle;
-let bob;
-let length;
-let angleV;
-let angleA;
-let origin;
-
+let pendulum;
 let gravity = 3;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
-    stroke(64);
-    strokeWeight(8);
-    fill(64);
-
-    angle = PI / 4;
-    bob = createVector();
-    length = height * 0.67;
-    angleV = 0;
-    angleA = 0.005;
-    origin = createVector(width / 2, 0);
+    pendulum = new Pendulum(width/2, height/5, PI/4, height/2, height/16, 8, 64);
 }
 
 function draw() {
     background(223);
 
-    angleA = -(gravity * sin(angle) / length);
-    angleV += angleA;
-    angle += angleV;
+    pendulum.render();
+    pendulum.update();
+}
 
-    bob.x = length * sin(angle) + origin.x;
-    bob.y = length * cos(angle) + origin.y;
+function Pendulum(x, y, angle, barLength, bobSize, barWeight=8, bobColor=64) {
+    this.origin = createVector(x, y);
+    this.angle = angle;
+    this.position = createVector();
+    this.barLength = barLength;
+    this.bobSize = bobSize;
+    this.barWeight = barWeight;
+    this.bobColor = bobColor;
+    this.angularVelocity = 0;
+    this.angularAcceleration = 0;
 
-    line(origin.x, origin.y, bob.x, bob.y);
-    circle(bob.x, bob.y, 64);
+    this.render = () => {
+        stroke(bobColor);
+        strokeWeight(barWeight);
+        fill(bobColor);
+        line(this.origin.x, this.origin.y, this.position.x, this.position.y);
+        circle(this.position.x, this.position.y, this.bobSize);
+    }
+
+    this.update = () => {
+        this.angularAcceleration = -(gravity * sin(this.angle) / this.barLength);
+        this.angularVelocity += this.angularAcceleration;
+        this.angle += this.angularVelocity;
+
+        this.position.x = this.barLength * sin(this.angle) + this.origin.x;
+        this.position.y = this.barLength * cos(this.angle) + this.origin.y;
+    }
 }
