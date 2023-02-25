b9579e475bd7b75997c69d721f7fd2439f371b5f
Michael Coyle <michael.david.coyle4@gmail.com>
2023-02-24 19:35:58 -0600

    render a spring-like pattern between anchor and bob

--- js/simple-spring.js
+++ js/simple-spring.js
@@ -3,20 +3,22 @@ let restLength = y;
 let k = 0.05;
 let velocity = 0;
 let damping = 0.99;
+let r;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    r = height / 16;
 }
 
 function draw() {
     background(191);
 
     stroke(96);
-    strokeWeight(8);
-    line(width / 2, 0, width / 2, y);
+    // strokeWeight(4);
+    renderSpring({x: width / 2, y: 0}, {x: width / 2, y: y});
 
     fill(96);
-    circle(width / 2, y, height / 16);
+    circle(width / 2, y, r);
 
     let x = y - restLength;
     // Hooke's Law
@@ -32,4 +34,18 @@ function draw() {
     y += velocity;
 
     velocity *= damping;
+}
+
+function renderSpring(p0, p1) {
+    let offset = 10;
+    let d = (p1.y - p0.y) - r / 2;
+    let s = d / 32;
+
+    for (let y = 0; y < d; y += s) {
+        push();
+        translate(p0.x, 0);
+        line(-offset, y, offset, y + s / 2);
+        line(offset, y + s / 2, -offset, y + s);
+        pop();
+    }
 }
